58a8261ca78859fe717a8acccba3e34e07db698c
mokhaleee <nkulyc@163.com>
2023-05-03 21:23:07 +0800

    myrrh style done

--- wizardry/EkrDragonHook/userspace/EkrDragonCommonIntro.c
+++ wizardry/EkrDragonHook/userspace/EkrDragonCommonIntro.c
@@ -8,7 +8,7 @@
 #include "ekrdragon.h"
 
 struct ProcEkrDragonCommonIntro {
-    struct ProcEkrDragon ekrdragon;
+    struct ProcEkrDragon drgproc;
     u8 fbanim_intro;
     u8 fbanim_main;
     u8 fbanim_exit;
@@ -16,13 +16,12 @@ struct ProcEkrDragonCommonIntro {
 
 void EkrDragReComm_PrepareBanimfx(struct ProcEkrDragonCommonIntro *proc)
 {
-    BattleAnim *banim = banim_data;
-    struct Anim *anim = proc->ekrdragon.anim;
-    proc->ekrdragon.timer = 0;
+    struct Anim *anim = proc->drgproc.anim;
+    proc->drgproc.timer = 0;
 
-    EkrPrepareBanimfx(anim, proc->fbanim_main);
-    sub_805A07C(anim, 0);
-    LZ77UnCompWram(banim[proc->fbanim_main].pal, gPalBackupEkrUnitMaybe);
+    EkrPrepareBanimfx(anim, proc->fbanim_intro);
+    BanimPrepareSpecificScript(anim, 0);
+    LZ77UnCompWram(banim_data[proc->fbanim_intro].pal, gPalBackupEkrUnitMaybe);
 
     if (GetAISSubjectId(anim) == EKR_BATTLE_LEFT)
         CpuFastCopy(gPalBackupEkrUnitMaybe, PAL_OBJ(0x7), 0x40);
@@ -35,19 +34,66 @@ void EkrDragReComm_PrepareBanimfx(struct ProcEkrDragonCommonIntro *proc)
 
 void EkrDragReComm_WaitForTransform(struct ProcEkrDragonCommonIntro *proc)
 {
-    struct Anim *anim = proc->ekrdragon.anim;
+    struct Anim *anim = proc->drgproc.anim;
 
-    if (++proc->ekrdragon.timer == 0x1A) {
+    if (++proc->drgproc.timer == 0x1A) {
         SomePlaySound_8071990(0xDC, 0x100);
-        sub_8071AB0(0xDC, anim->xPosition, 1);
+        M4aPlayWithPostionCtrl(0xDC, anim->xPosition, 1);
     }
-    
+
     if (ANINS_GET_TYPE(*anim->pScrCurrent) == ANIM_INS_TYPE_STOP) {
         SomePlaySound_8071990(0xDE, 0x100);
-        sub_8071AB0(0xDE, anim->xPosition, 1);
-        EkrPrepareBanimfx(anim, proc->fbanim_intro);
+        M4aPlayWithPostionCtrl(0xDE, anim->xPosition, 1);
+        EkrPrepareBanimfx(anim, proc->fbanim_main);
 
-        proc->ekrdragon.timer = 0;
+        proc->drgproc.timer = 0;
+        Proc_Break(proc);
+    }
+}
+
+void EkrDragReComm_OnBanimExit(struct ProcEkrDragonCommonIntro *proc)
+{
+    struct Anim *anim = proc->drgproc.anim;
+    BattleAnim *banim = banim_data;
+
+#if 0
+    if (GetEfxHp(2 * gEfxPairHpBufOffset[GetAISSubjectId(anim)] + GetAISSubjectId(anim)) <= 0) {
+        proc->drgproc.timer = 0;
+        EkrPrepareBanimfx(anim, 0xC7 - 1);
+        gEkrSpellAnimIndexLutMaybe[0] = -1;
+        Proc_Break(proc);
+        return;
+    }
+#endif
+
+    SomePlaySound_8071990(0xDD, 0x100);
+    M4aPlayWithPostionCtrl(0xDD, anim->xPosition, 1);
+
+    EkrPrepareBanimfx(anim, proc->fbanim_exit);
+    BanimPrepareSpecificScript(anim, 0);
+    Proc_Break(proc);
+    LZ77UnCompWram(banim[proc->fbanim_exit].pal, gPalBackupEkrUnitMaybe);
+
+    if (GetAISSubjectId(anim) == EKR_BATTLE_LEFT)
+        CpuFastCopy(gPalBackupEkrUnitMaybe, PAL_OBJ(0x7), 0x40);
+    else
+        CpuFastCopy(gPalBackupEkrUnitMaybe, PAL_OBJ(0x9), 0x40);
+
+    EnablePaletteSync();
+}
+
+void EkrDragReComm_OnEkrExit(struct ProcEkrDragonCommonIntro *proc)
+{
+    struct Anim *anim = proc->drgproc.anim;
+    if (ANINS_GET_TYPE(*anim->pScrCurrent) == ANIM_INS_TYPE_STOP) {
+        EkrPrepareBanimfx(anim, proc->fbanim_exit);
+        AddEkrDragonStatusAttr(proc->drgproc.anim, EKRDRGON_ATTR_END);
+
+        if (GetAISSubjectId(anim) == EKR_BATTLE_LEFT)
+            SetEkrDragonStatusType(gAnims[0], EKRDRGON_TYPE_NORMAL);
+        else
+            SetEkrDragonStatusType(gAnims[2], EKRDRGON_TYPE_NORMAL);
+
         Proc_Break(proc);
     }
 }
@@ -60,15 +106,18 @@ const struct ProcCmd ProScr_EkrDragonReworkCommonIntro[] = {
 PROC_LABEL(0),
     PROC_REPEAT(EkrMyr_TrigerForPrepared),
     PROC_REPEAT(EkrMyr_InBattleIdle),
+
+    PROC_REPEAT(EkrDragReComm_OnBanimExit),
+    PROC_REPEAT(EkrDragReComm_OnEkrExit),
     PROC_END,
 };
 
 void EkrDragonCommonIntro(struct Anim *anim, u32 priv)
 {
     struct EkrDragonFrameLut _priv = {
-        .fbanim_intro = priv & 0xFF,
-        .fbanim_main = (priv >> 8) & 0xFF,
-        .fbanim_exit = (priv >> 16) & 0xFF,
+        .fbanim_intro = (priv & 0xFF),
+        .fbanim_main = ((priv >> 8) & 0xFF),
+        .fbanim_exit = ((priv >> 16) & 0xFF),
     };
 
     struct EkrDragonStatus *ekrsp = GetEkrDragonStatus(anim);
@@ -77,8 +126,8 @@ void EkrDragonCommonIntro(struct Anim *anim, u32 priv)
     ekrsp->proc = proc;
     AddEkrDragonStatusAttr(anim, EKRDRGON_ATTR_START);
     ekrsp->anim = anim;
-    proc->ekrdragon.anim = anim;
-    proc->ekrdragon.timer = 0;
+    proc->drgproc.anim = anim;
+    proc->drgproc.timer = 0;
 
     proc->fbanim_intro = _priv.fbanim_intro;
     proc->fbanim_main = _priv.fbanim_main;
