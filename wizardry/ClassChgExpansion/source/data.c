be7cb6a9fc3d578cb0d87511b842f5d22a107659
MokhaLeee <nkulyc@163.com>
2023-08-05 10:51:40 +0000

    classchg: add feb patches

--- wizardry/ClassChgExpansion/source/data.c
+++ wizardry/ClassChgExpansion/source/data.c
@@ -4,25 +4,20 @@
 
 #include "ClassChgExpansion.h"
 
-/* Magic string for FEBuilder patch */
-const char PromoJidLutExpaMagic[] = "PromoJidLutExpa";
 const u8 gPromoJidLutExpa[0x100][0x4] = {
     [CLASS_JOURNEYMAN] = {
-        CLASS_JOURNEYMAN_T1,
         CLASS_MAGE,
         CLASS_SHAMAN,
         CLASS_MONK,
     },
 
     [CLASS_RECRUIT] = {
-        CLASS_RECRUIT_T1,
         CLASS_CAVALIER_F,
         CLASS_ARMOR_KNIGHT_F,
         CLASS_PEGASUS_KNIGHT
     },
 
     [CLASS_PUPIL] = {
-        CLASS_PUPIL_T1,
         CLASS_FIGHTER,
         CLASS_BRIGAND
     },
@@ -40,20 +35,35 @@ const u8 gPromoJidLutExpa[0x100][0x4] = {
     },
 };
 
+const char PromoJidLutExpaMagic[] = "PromoJidLutExpa"; /* for FEBuilder patch */
+
 static const struct ClassChgExpaMod gClassChgExpaMods[] = {
     {
-        .jid_promo = CLASS_PEGASUS_KNIGHT,
-        .jid_old = CLASS_JOURNEYMAN,
+        .jid_promo = CLASS_RECRUIT_T1,
+        .jid_old = CLASS_RECRUIT,
         .pid = CHARACTER_AMELIA,
+        .item = 0,
+        .flag = 0,
+    },
+    {
+        .jid_promo = CLASS_JOURNEYMAN_T1,
+        .jid_old = CLASS_JOURNEYMAN,
+        .pid = CHARACTER_ROSS,
+        .item = 0,
+        .flag = 0,
+    },
+    {
+        .jid_promo = CLASS_PUPIL_T1,
+        .jid_old = CLASS_PUPIL,
+        .pid = CHARACTER_EWAN,
         .item = 0,
         .flag = 0,
     },
     {0}
 };
 
-/* Magic string for FEBuilder patch */
-const char ClassChgExpaModsMagic[] = "ClassChgExpaMod";
 struct ClassChgExpaMod const * const gpClassChgExpaMods = gClassChgExpaMods;
+const char ClassChgExpaModsMagic[] = "ClassChgExpaMod"; /* for FEBuilder patch */
 
 static const struct TraineeDataRe gTraineesRe[] = {
     { CLASS_JOURNEYMAN, 10 },
@@ -62,6 +72,5 @@ static const struct TraineeDataRe gTraineesRe[] = {
     {0}
 };
 
-/* Magic string for FEBuilder patch */
-const char TraineesReMagic[] = "TraineesRee";
 struct TraineeDataRe const * const gpTraineesRe = gTraineesRe;
+const char TraineesReMagic[] = "TraineesRee"; /* for FEBuilder patch */
