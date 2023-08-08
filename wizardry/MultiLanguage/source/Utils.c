75d7589dd36e2ee0aeb93235b1499e44a53ead91
MokhaLeee <nkulyc@163.com>
2023-08-08 07:00:29 +0000

    update utf8 decode routine

--- wizardry/MultiLanguage/source/Utils.c
+++ wizardry/MultiLanguage/source/Utils.c
@@ -21,79 +21,72 @@ int GetLenUtf8(const u16 * utf8_in)
     return -1;
 }
 
-int DecodeUtf8(const u16 * utf8_buf, u32 * out, int * len)
+int DecodeUtf8(const u16 * utf8_in, u32 * unicode_out, int * len)
 {
-    u16 tmp[4];
+    u32 unicod;
 
-    switch (GetLenUtf8(utf8_buf))
+    switch (GetLenUtf8(utf8_in))
     {
     case 1:
-        *out = *utf8_buf;
+        *unicode_out = *utf8_in;
         *len = 1;
         return 0;
 
     case 2:
-        tmp[0] = utf8_buf[0] & 0x00011111;
-        tmp[1] = utf8_buf[1] & 0x00111111;
+        unicod = utf8_in[0] & 0x00011111;
+        unicod = (unicod << 0x6) | (utf8_in[1] & 0x00111111);
 
-        *out = tmp[0];
-        *out = (*out << 0x6) | tmp[1];
+        *unicode_out = unicod;
         *len = 2;
         return 0;
 
     case 3:
-        tmp[0] = utf8_buf[0] & 0x00001111;
-        tmp[1] = utf8_buf[1] & 0x00111111;
-        tmp[2] = utf8_buf[2] & 0x00111111;
+        unicod = utf8_in[0] & 0x00001111;
+        unicod = (unicod << 0x6) | (utf8_in[1] & 0x00111111);
+        unicod = (unicod << 0x6) | (utf8_in[2] & 0x00111111);
 
-        *out = tmp[0];
-        *out = (*out << 0x6) | tmp[1];
-        *out = (*out << 0x6) | tmp[2];
+        *unicode_out = unicod;
         *len = 3;
         return 0;
 
     case 4:
-        tmp[0] = utf8_buf[0] & 0x00000111;
-        tmp[1] = utf8_buf[1] & 0x00111111;
-        tmp[2] = utf8_buf[2] & 0x00111111;
-        tmp[3] = utf8_buf[3] & 0x00111111;
+        unicod = utf8_in[0] & 0x00000111;
+        unicod = (unicod << 0x6) | (utf8_in[1] & 0x00111111);
+        unicod = (unicod << 0x6) | (utf8_in[2] & 0x00111111);
+        unicod = (unicod << 0x6) | (utf8_in[3] & 0x00111111);
 
-        *out = tmp[0];
-        *out = (*out << 0x6) | tmp[1];
-        *out = (*out << 0x6) | tmp[2];
-        *out = (*out << 0x6) | tmp[3];
+        *unicode_out = unicod;
         *len = 4;
         return 0;
 
     default:
-        *out = 0;
+        *unicode_out = 0;
         *len = 0;
         return -1;
     }
 }
 
-struct Glyph const * const gGlyphsItem[16] = {
-    [0] = (void *)0x858C7EC,
+struct Glyph ** const gGlyphsItem[16] = {
+    [0] = TextGlyphs_System,
 };
 
-struct Glyph const * const gGlyphsText[16] = {
-    [0] = (void *)0x858F6F4,
+struct Glyph ** const gGlyphsText[16] = {
+    [0] = TextGlyphs_Talk,
 };
 
 const struct Glyph * GetCharGlyphUnicode(u32 unicode_ch, int * len, int type)
 {
-    struct Glyph const * glyph;
+    struct Glyph ** glyph_grp, * glyph;
     int grp = unicode_ch >> 0x10;
     int hi = (unicode_ch >> 0x8) & 0xFF;
     int lo = unicode_ch & 0xFF;
-    struct Glyph const * glyph_grp;
-    
+
     if (type == 0)
         glyph_grp = gGlyphsItem[grp];
     else
         glyph_grp = gGlyphsText[grp];
 
-    for(glyph = &glyph_grp[lo]; glyph != NULL; glyph = glyph->sjisNext)
+    for(glyph = glyph_grp[lo]; glyph != NULL; glyph = glyph->sjisNext)
     {
         if (glyph->sjisByte1 == hi)
         {
