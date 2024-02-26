002cc2481f6fffac0241776f567917bfe0e04e48
MokhaLeee <nkulyc@163.com>
2024-02-26 16:07:17 +0000

    add EventScr_CallThunderfxAtPosition

---
+++ config.h
@@ -0,0 +1,3 @@
+// #define TEST_CallThunderfx
+#define TEST_CallThunderfxAtPosition
+#define TEST_SpawnAssassinfx
--- wizardry/EventThunderfx/Event/Thunderfx.c
+++ wizardry/EventThunderfx/Event/Thunderfx.c
@@ -16,11 +16,30 @@ static void CallEventThunderfxForUnitSlot2(struct EventEngineProc * proc)
 }
 
 const EventScr EventScr_CallThunderfx[] = {
-    NoFade
     STARTFADE
     EvtColorFadeSetup(0x0, 0x20, 8, 128, 128, 128) // ENOSUPP in EAstdlib
     STAL(30)
     ASMC(CallEventThunderfxForUnitSlot2)
+    STAL(60)
+    EvtColorFadeSetup(0x0, 0x20, 4, 256, 256, 256) // ENOSUPP in EAstdlib
+    ENDA
+};
+
+static void CallEventThunderfxAtPosition(struct EventEngineProc * proc)
+{
+    int x, y;
+
+    x = gEventSlots[3] * 16 - gBmSt.camera.x;
+    y = gEventSlots[4] * 16 - gBmSt.camera.y;
+
+    StartBmThunderfx(x, y, proc);
+}
+
+const EventScr EventScr_CallThunderfxAtPosition[] = {
+    STARTFADE
+    EvtColorFadeSetup(0x0, 0x20, 8, 128, 128, 128) // ENOSUPP in EAstdlib
+    STAL(30)
+    ASMC(CallEventThunderfxAtPosition)
     STAL(60)
     EvtColorFadeSetup(0x0, 0x20, 4, 256, 256, 256) // ENOSUPP in EAstdlib
     ENDA
