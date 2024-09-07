38f8f4d3d42382e9a80747f893623d77a7b8d6d9
MokhaLeee <nkulyc@163.com>
2024-09-07 16:22:14 +0000

    update tools

--- tools/build-tools.sh
+++ tools/build-tools.sh
@@ -3,10 +3,10 @@
 sudo apt-get -y install binutils-arm-none-eabi ctags gcc-arm-none-eabi build-essential cmake re2c ghc cabal-install libghc-vector-dev libghc-juicypixels-dev python3-pip pkg-config libpng* bsdiff moreutils
 pip install pyelftools PyInstaller tmx six
 
-cd Tools
-git clone https://github.com/MokhaLeee/FE-CLib-Mokha.git Tools/FE-CLib-Mokha
-git clone https://github.com/StanHash/EventAssembler.git --recursive Tools/EventAssembler
-git clone https://github.com/StanHash/FE-PyTools.git --recursive Tools/FE-PyTools
+cd tools
+git clone https://github.com/MokhaLeee/FE-CLib-Mokha.git
+git clone https://github.com/StanHash/EventAssembler.git --recursive
+git clone https://github.com/StanHash/FE-PyTools.git --recursive
 
 wget https://apt.devkitpro.org/install-devkitpro-pacman
 chmod +x ./install-devkitpro-pacman
@@ -26,4 +26,4 @@ sudo ./dotnet-install.sh --channel 6.0
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
-cd Tools/EventAssembler && ./build.sh && cd ../..
+cd EventAssembler && ./build.sh && cd ../..
