b57b4b15a9d29b39e00a0bb641b1069bc52e0a1f
MokhaLeee <nkulyc@163.com>
2024-10-12 06:18:12 +0000

    Fix code style

--- wizardry/BanimHpDrainFix/Source/BanimHpDrainFix.c
+++ wizardry/BanimHpDrainFix/Source/BanimHpDrainFix.c
@@ -3,15 +3,16 @@
 extern void * const prBattleHitArray;
 extern u8 BattleHitArrayWidth;
 
-void StartBattleAnimHitEffectsDefault(struct Anim *anim, int type)
+void StartBattleAnimHitEffectsDefault(struct Anim * anim, int type)
 {
-	int round = anim->nextRoundId - 1;
-	struct BattleHit * hit = (prBattleHitArray + BattleHitArrayWidth * round);
+    int round = anim->nextRoundId - 1;
+    struct BattleHit * hit = (prBattleHitArray + BattleHitArrayWidth * round);
 
-	if (hit->attributes & BATTLE_HIT_ATTR_HPSTEAL) {
-		StartBattleAnimResireHitEffects(anim, type);
-		return;
-	}
+    if (hit->attributes & BATTLE_HIT_ATTR_HPSTEAL)
+    {
+        StartBattleAnimResireHitEffects(anim, type);
+        return;
+    }
 
-	StartBattleAnimHitEffects(anim, type, 3, 4);
+    StartBattleAnimHitEffects(anim, type, 3, 4);
 }
